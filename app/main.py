325c46de5716ba1d279f55283dbc1a57e12e46b0
Arezki <ait.mouheb.arezki@gmail.com>
2026-01-27 20:22:00 +0100

    - add allowed origines and verbs in CORS policy (dev included)

--- app/main.py
+++ app/main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.common import common_router
 from app.routers.orders import orders_router
 
-
+origins = [
+    "http://192.168.1.157:5173",
+    "https://pizza42-spa-f7bb8ac15f77.herokuapp.com",
+]
 
 app = FastAPI(
     title="Pizza 42 Orders API",
     description="Backend API for Pizza 42 pizza orders",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["Authorization", "Content-Type"]
 )
 
 app.include_router(common_router)
