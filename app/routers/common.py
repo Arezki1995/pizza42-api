1cee561731f9f3eb6e9977ff730e04d02cad15c6
AIT MOUHEB Arezki <ait.mouheb.arezki@gmail.com>
2026-01-31 20:53:40 +0100

    remove unused import

--- app/routers/common.py
+++ app/routers/common.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
-from fastapi.responses import FileResponse, RedirectResponse
-
+from fastapi.responses import RedirectResponse
 
 common_router = APIRouter(tags=["Common"])
 
@@ -11,5 +10,3 @@ def root():
 @common_router.get("/health")
 def health_check():
     return {"status": "ok"}
-
-
