c625f8c9a34a735b7f70cf488e8449dd238db4f6
AIT MOUHEB Arezki <ait.mouheb.arezki@gmail.com>
2026-02-04 22:14:05 +0100

    Remove the no longer necessary health-check

--- app/routers/common.py
+++ app/routers/common.py
@@ -6,7 +6,3 @@ common_router = APIRouter(tags=["Common"])
 @common_router.get("/", include_in_schema=False)
 def root():
     return RedirectResponse(url="/docs")
-
-@common_router.get("/health")
-def health_check():
-    return {"status": "ok"}
